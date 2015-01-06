1179ba8c267e6852e536f6ae8d98b946e97fc18a
Ali Bozorgkhan <alibozorgkhan@gmail.com>
2015-01-06 14:32:25 -0800

    added dummy settings file

---
+++ utils/settings.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+#   CACHEOO
+CACHE_PATH = ""
+CACHE_EXPIRE = 60 * 60 * 24 * 33  # 30 days
+
+#   DBOO
+POSTGRES_CONFIG = {
+    'host': '',
+    'user': '',
+    'password': '',
+    'dbname': ''
+}
+
+#   WEBOO
+DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.8,fa;q=0.6',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'Referer': 'http://www.google.com',
+    'Cache-Control': 'max-age=0',
+}
+
+PROXY_USERNAME = ""
+PROXY_PASSWORD = ""
+PROXIES = """
+PROXIES LINE BY LINE
+"""
